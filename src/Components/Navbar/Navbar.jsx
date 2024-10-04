df583b3b1f1825ce335349004f5904e0e4db15bd
unknown <tarunsai2203@gmail.com>
2024-10-04 15:45:43 +0530

    added cart-items and made all the pages responsive according to screensize

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown_icon from '../Assets/nav_dropdown.png'
+
 const Navbar = () => {
 
   const [menu,setMenu] = useState("shop");
+  const {getTotalCartItems} = useContext(ShopContext);
+  const menuRef = useRef();
+  
+  const dropdownToggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
@@ -13,8 +23,8 @@ const Navbar = () => {
         <img src={logo} alt=''/>
         <p>SHOPPER</p>
       </div>
-
-      <ul className='nav-menu'>
+      <img className='nav-dropdown' onClick={dropdownToggle} src={nav_dropdown_icon} alt="" />
+      <ul ref={menuRef} className='nav-menu'>
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: "none"}} to="/">Shop</Link>{menu === "shop" ? <hr/>:<></>}</li>
         <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration: "none"}} to="/men" >Men</Link>{menu === "men" ? <hr/>:<></>}</li>
         <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration: "none"}} to="/women">Women</Link>{menu === "women" ? <hr/>:<></>}</li>
@@ -23,7 +33,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <Link to="/login"><button>Login</button></Link>
         <Link to="/cart"><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   )
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
         <h1>POPULAR IN WOMEN</h1>
         <hr/>
         <div className="popular-item">
-            {data_product.map((item,i)=>{
+            {data_product.filter((item) => item.category === "women").map((item,i)=>{
                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
         </div>
